c28a946a2be80d6824375db636327b059a3ea7bd
syntactic-salt <kennethpereira@gmail.com>
2020-11-20 23:42:56 -0500

    enhance error messages

--- source/getConfiguration.js
+++ source/getConfiguration.js
@@ -3,13 +3,16 @@ const fs = require("fs");
 const getConfiguration = () => {
     let configuration = {};
 
-    if (fs.existsSync("./.browserslist-nr.json")) {
+    if (fs.existsSync("./.browserslist-new-relic.json")) {
         try {
-            configuration = JSON.parse(fs.readFileSync("./.browserslist-nr.json"));
+            configuration = JSON.parse(fs.readFileSync("./.browserslist-new-relic.json"));
         } catch (error) {
             console.error("Your configuration file could not be parsed. Check to make sure it contains valid JSON.");
             process.exit(1);
         }
+    } else {
+        console.error("Your configuration file is missing (.browserslist-new-relic.json).");
+        process.exit(1);
     }
 
     return configuration;
