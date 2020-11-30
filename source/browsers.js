211a2626e2bd239b5a29c610919c3f9296a696a4
syntactic-salt <kennethpereira@gmail.com>
2020-11-29 20:17:10 -0500

    add additional browser mappings

--- source/browsers.js
+++ source/browsers.js
@@ -1,5 +1,18 @@
 const browsers = {
+    "Android Browser:Mobile": "android",
     "Chrome:Desktop": "chrome",
+    "Chrome:Mobile": "and_chr",
+    "Chrome:Tablet": "and_chr",
+    "Firefox:Desktop": "firefox",
+    "Firefox:Mobile": "and_ff",
+    "IE:Desktop": "ie",
+    "Microsoft Edge:Desktop": "edge",
+    "Opera:Desktop": "opera",
+    "Opera Mini:Mobile": "op_mini",
+    "Opera Mobile:Mobile": "op_mob",
+    "Safari:Desktop": "safari",
+    "Safari:Mobile": "ios_saf",
+    "Safari:Tablet": "ios_saf",
 };
 
 module.exports = browsers;
