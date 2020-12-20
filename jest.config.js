ee006f2d746663723c0f95a2281efaa7ceb8bb52
syntactic-salt <kennethpereira@gmail.com>
2020-12-20 01:43:50 -0500

    configure test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -34,15 +34,22 @@ module.exports = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
+  coverageReporters: [
   //   "json",
-  //   "text",
+    "text",
   //   "lcov",
   //   "clover"
-  // ],
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+      "global": {
+        "branches": 100,
+        "functions": 100,
+        "lines": 100,
+        "statements": 100
+      }
+    },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
