b9c1621c7d2d3e99609627d2e069b98e9b93bc54
syntactic-salt <kennethpereira@gmail.com>
2021-09-06 13:58:45 -0400

    switch to yargs, increase test coverage, and add version

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const { getOptions, optionDefinitions } = require("./lib/getOptions");
+const yargs = require("yargs/yargs");
+const { hideBin } = require('yargs/helpers')
+const getOptions = require("./lib/getOptions");
 const getQueryResults = require("./lib/getQueryResults");
 const transformResults = require("./lib/transformResults");
 
-let options;
+const envVarPrefix = "BROWSERSLIST_NEW_RELIC_";
+const argv = yargs(hideBin(process.argv))
+    .option("accountId", {
+        describe: "New Relic account ID",
+        demandOption: !process.env[`${envVarPrefix}ACCOUNT_ID`],
+        type: "string",
+    })
+    .option("apiKey", {
+        describe: "New Relic Insights query API key",
+        demandOption: !process.env[`${envVarPrefix}API_KEY`],
+        type: "string",
+    })
+    .option("appId", {
+        describe: "New Relic Browser application ID",
+        demandOption: !process.env[`${envVarPrefix}APP_ID`],
+        type: "string",
+    })
+    .option("debug", {
+        describe: "Turn on debugging output",
+        default: false,
+        type: "boolean",
+    })
+    .option("duration", {
+        describe: "Days of browser usage to fetch",
+        default: 7,
+        type: "number"
+    })
+    .help()
+    .argv;
 
-try {
-    options = getOptions();
-} catch (error) {
-    console.error(error, error.stack);
-    process.exit(1);
-}
-
-const { apiKey, appId, accountId, duration } = options;
+const { apiKey, appId, accountId, duration, debug } = getOptions(argv);
 const query = `SELECT count(*) FROM PageView FACET userAgentName, userAgentVersion, deviceType SINCE ${duration} DAYS AGO WHERE appId = ${appId} LIMIT MAX`;
 
-if (process.env[optionDefinitions.debug.envVar]) {
+if (debug) {
     console.debug("\nNEW RELIC QUERY - NRQL");
     console.debug(query);
 }
@@ -28,9 +51,9 @@ getQueryResults(query, { apiKey, accountId })
             console.error("Error getting results from New Relic.");
             console.error(nrResults.error);
             process.exit(1);
+        } else {
+            const browserslistStats = JSON.stringify(transformResults(nrResults), null, 2);
+            fs.writeFileSync("./browserslist-stats.json", browserslistStats);
+            console.log("Finished generating browserslist-stats.json");
         }
-
-        const browserslistStats = JSON.stringify(transformResults(nrResults), null, 2);
-        fs.writeFileSync("./browserslist-stats.json", browserslistStats);
-        console.log("Finished generating browserslist-stats.json");
     });
