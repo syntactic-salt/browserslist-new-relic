2f9c99a1d35598cbaae25f3fa9acb459e64086da
syntactic-salt <kennethpereira@gmail.com>
2021-09-11 18:56:05 -0400

    switch to single quotes

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,8 +1,8 @@
-const fs = require("fs");
-const getQueryResults = require("../lib/getQueryResults");
-const getOptions = require("../lib/getOptions");
+const fs = require('fs');
+const getQueryResults = require('../lib/getQueryResults');
+const getOptions = require('../lib/getOptions');
 
-jest.mock("yargs/yargs", () => {
+jest.mock('yargs/yargs', () => {
     const mockYargs = () => {
         return {
             option: mockYargs,
@@ -13,32 +13,32 @@ jest.mock("yargs/yargs", () => {
 
     return jest.fn().mockImplementation(mockYargs);
 });
-jest.mock("../lib/getQueryResults");
-jest.mock("../lib/getOptions", () => {
+jest.mock('../lib/getQueryResults');
+jest.mock('../lib/getOptions', () => {
     return jest.fn().mockImplementation(() => {
         return {
             debug: false,
-            apiKey: "08sf9",
-            accountId: "34521432",
-            appId: "6543",
+            apiKey: '08sf9',
+            accountId: '34521432',
+            appId: '6543',
             duration: 7,
         };
     });
 });
 
 beforeAll(() => {
-    jest.spyOn(console, "log").mockImplementation(() => {});
-    jest.spyOn(console, "debug").mockImplementation(() => {});
-    jest.spyOn(console, "info").mockImplementation((() => {}));
-    jest.spyOn(fs, "writeFileSync").mockImplementation(() => {});
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+    jest.spyOn(console, 'debug').mockImplementation(() => {});
+    jest.spyOn(console, 'info').mockImplementation((() => {}));
+    jest.spyOn(fs, 'writeFileSync').mockImplementation(() => {});
 });
 
 beforeEach(() => {
     getQueryResults.mockResolvedValue({
         totalResult: { results: [{ count: 100 }] },
         facets: [
-            { name: ["Chrome", "86", "Desktop"], results: [{ count: 25 }] },
-            { name: ["Chrome", "84", "Desktop"], results: [{ count: 75 }] },
+            { name: ['Chrome', '86', 'Desktop'], results: [{ count: 25 }] },
+            { name: ['Chrome', '84', 'Desktop'], results: [{ count: 75 }] },
         ],
     });
 });
@@ -47,31 +47,31 @@ afterEach(() => {
     jest.clearAllMocks();
 });
 
-describe("main file", () => {
-    test("exits when failing to get new relic results", async () => {
-        getQueryResults.mockRejectedValueOnce(new Error("Have you tried turning it off and on again?"));
-        jest.spyOn(console, "error").mockImplementation(() => {});
-        jest.spyOn(process, "exit"). mockImplementation(() => {});
+describe('main file', () => {
+    test('exits when failing to get new relic results', async () => {
+        getQueryResults.mockRejectedValueOnce(new Error('Have you tried turning it off and on again?'));
+        jest.spyOn(console, 'error').mockImplementation(() => {});
+        jest.spyOn(process, 'exit'). mockImplementation(() => {});
         jest.isolateModules(() => {
-            require("../index");
+            require('../index');
         });
         await new Promise((resolve) => setTimeout(resolve, 50));
         expect(console.error).toHaveBeenCalledTimes(1);
         expect(process.exit).toHaveBeenCalledTimes(1);
     });
 
-    test("writes a custom usage data file", async () => {
+    test('writes a custom usage data file', async () => {
         jest.isolateModules(() => {
-            require("../index");
+            require('../index');
         });
         await new Promise((resolve) => setTimeout(resolve, 50));
         expect(fs.writeFileSync).toHaveBeenCalledTimes(1);
     });
 
-    test("outputs debugging info", () => {
+    test('outputs debugging info', () => {
         getOptions.mockReturnValueOnce({ ...getOptions(), ...{ debug: true } });
         jest.isolateModules(() => {
-            require("../index");
+            require('../index');
         });
         expect(console.debug).toHaveBeenCalledTimes(2);
     });
