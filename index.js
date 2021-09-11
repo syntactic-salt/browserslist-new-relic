fa29e80fe4bbdc23f8508a6d34d2c8c3ce99cb08
syntactic-salt <kennethpereira@gmail.com>
2021-09-11 18:56:05 -0400

    switch to single quotes

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const yargs = require("yargs/yargs");
+const fs = require('fs');
+const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers')
-const getOptions = require("./lib/getOptions");
-const getQueryResults = require("./lib/getQueryResults");
-const transformResults = require("./lib/transformResults");
+const getOptions = require('./lib/getOptions');
+const getQueryResults = require('./lib/getQueryResults');
+const transformResults = require('./lib/transformResults');
 
-const envVarPrefix = "BROWSERSLIST_NEW_RELIC_";
+const envVarPrefix = 'BROWSERSLIST_NEW_RELIC_';
 const argv = yargs(hideBin(process.argv))
-    .option("accountId", {
-        describe: "New Relic account ID",
+    .option('accountId', {
+        describe: 'New Relic account ID',
         demandOption: !process.env[`${envVarPrefix}ACCOUNT_ID`],
-        type: "string",
+        type: 'string',
     })
-    .option("apiKey", {
-        describe: "New Relic Insights query API key",
+    .option('apiKey', {
+        describe: 'New Relic Insights query API key',
         demandOption: !process.env[`${envVarPrefix}API_KEY`],
-        type: "string",
+        type: 'string',
     })
-    .option("appId", {
-        describe: "New Relic Browser application ID",
+    .option('appId', {
+        describe: 'New Relic Browser application ID',
         demandOption: !process.env[`${envVarPrefix}APP_ID`],
-        type: "string",
+        type: 'string',
     })
-    .option("debug", {
-        describe: "Turn on debugging output",
+    .option('debug', {
+        describe: 'Turn on debugging output',
         default: false,
-        type: "boolean",
+        type: 'boolean',
     })
-    .option("duration", {
-        describe: "Days of browser usage to fetch",
+    .option('duration', {
+        describe: 'Days of browser usage to fetch',
         default: 7,
-        type: "number"
+        type: 'number'
     })
     .help()
     .argv;
@@ -42,18 +42,18 @@ const argv = yargs(hideBin(process.argv))
     const query = `SELECT count(*) FROM PageView FACET userAgentName, userAgentVersion, deviceType SINCE ${duration} DAYS AGO WHERE appId = ${appId} LIMIT MAX`;
 
     if (debug) {
-        console.debug("\nNEW RELIC QUERY - NRQL");
+        console.debug('\nNEW RELIC QUERY - NRQL');
         console.debug(query);
     }
 
     try {
         const newRelicResults = await getQueryResults(query, {apiKey, accountId});
         const browserslistStats = JSON.stringify(transformResults(newRelicResults), null, 2);
-        fs.writeFileSync("./browserslist-stats.json", browserslistStats);
+        fs.writeFileSync('./browserslist-stats.json', browserslistStats);
     } catch (error) {
         console.error(error, error.stack);
         process.exit(1);
     }
 
-    console.info("Finished generating browserslist-stats.json");
+    console.info('Finished generating browserslist-stats.json');
 }());
--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     // bail: 0,
 
     // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\kpereira\\AppData\\Local\\Temp\\jest",
+    // cacheDirectory: 'C:\\Users\\kpereira\\AppData\\Local\\Temp\\jest',
 
     // Automatically clear mock calls and instances between every test
     clearMocks: true,
@@ -23,32 +23,32 @@ module.exports = {
     // collectCoverageFrom: undefined,
 
     // The directory where Jest should output its coverage files
-    coverageDirectory: "coverage",
+    coverageDirectory: 'coverage',
 
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
+    //   '\\\\node_modules\\\\'
     // ],
 
     // Indicates which provider should be used to instrument code for coverage
-    coverageProvider: "babel",
+    coverageProvider: 'babel',
 
     // A list of reporter names that Jest uses when writing coverage reports
     coverageReporters: [
-        //   "json",
-        "text",
-        "html",
-        //   "lcov",
-        //   "clover"
+        //   'json',
+        'text',
+        'html',
+        //   'lcov',
+        //   'clover'
     ],
 
     // An object that configures minimum threshold enforcement for coverage results
     coverageThreshold: {
-        "global": {
-            "branches": 90,
-            "functions": 90,
-            "lines": 90,
-            "statements": 90
+        'global': {
+            'branches': 90,
+            'functions': 90,
+            'lines': 90,
+            'statements': 90
         }
     },
 
@@ -71,21 +71,21 @@ module.exports = {
     // globals: {},
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-    // maxWorkers: "50%",
+    // maxWorkers: '50%',
 
     // An array of directory names to be searched recursively up from the requiring module's location
     // moduleDirectories: [
-    //   "node_modules"
+    //   'node_modules'
     // ],
 
     // An array of file extensions your modules use
     // moduleFileExtensions: [
-    //   "js",
-    //   "json",
-    //   "jsx",
-    //   "ts",
-    //   "tsx",
-    //   "node"
+    //   'js',
+    //   'json',
+    //   'jsx',
+    //   'ts',
+    //   'tsx',
+    //   'node'
     // ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
@@ -98,7 +98,7 @@ module.exports = {
     // notify: false,
 
     // An enum that specifies notification mode. Requires { notify: true }
-    // notifyMode: "failure-change",
+    // notifyMode: 'failure-change',
 
     // A preset that is used as a base for Jest's configuration
     // preset: undefined,
@@ -126,11 +126,11 @@ module.exports = {
 
     // A list of paths to directories that Jest should use to search for files in
     // roots: [
-    //   "<rootDir>"
+    //   '<rootDir>'
     // ],
 
     // Allows you to use a custom runner instead of Jest's default test runner
-    // runner: "jest-runner",
+    // runner: 'jest-runner',
 
     // The paths to modules that run some code to configure or set up the testing environment before each test
     // setupFiles: [],
@@ -145,7 +145,7 @@ module.exports = {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: "node",
+    testEnvironment: 'node',
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
@@ -155,13 +155,13 @@ module.exports = {
 
     // The glob patterns Jest uses to detect test files
     // testMatch: [
-    //   "**/__tests__/**/*.[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).[tj]s?(x)"
+    //   '**/__tests__/**/*.[jt]s?(x)',
+    //   '**/?(*.)+(spec|test).[tj]s?(x)'
     // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
+    //   '\\\\node_modules\\\\'
     // ],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
@@ -171,21 +171,21 @@ module.exports = {
     // testResultsProcessor: undefined,
 
     // This option allows use of a custom test runner
-    // testRunner: "jasmine2",
+    // testRunner: 'jasmine2',
 
     // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-    // testURL: "http://localhost",
+    // testURL: 'http://localhost',
 
-    // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
-    // timers: "real",
+    // Setting this value to 'fake' allows the use of fake timers for functions such as 'setTimeout'
+    // timers: 'real',
 
     // A map from regular expressions to paths to transformers
     // transform: undefined,
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\",
-    //   "\\.pnp\\.[^\\\\]+$"
+    //   '\\\\node_modules\\\\',
+    //   '\\.pnp\\.[^\\\\]+$'
     // ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
