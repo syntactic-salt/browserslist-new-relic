96b7d4f6f186e2ff49e5be5ca2e6aa00164ae8ca
syntactic-salt <kennethpereira@gmail.com>
2021-09-11 18:41:51 -0400

    mute console.info during text execution

--- __tests__/index.js
+++ __tests__/index.js
@@ -29,6 +29,7 @@ jest.mock("../lib/getOptions", () => {
 beforeAll(() => {
     jest.spyOn(console, "log").mockImplementation(() => {});
     jest.spyOn(console, "debug").mockImplementation(() => {});
+    jest.spyOn(console, "info").mockImplementation((() => {}));
     jest.spyOn(fs, "writeFileSync").mockImplementation(() => {});
 });
 
