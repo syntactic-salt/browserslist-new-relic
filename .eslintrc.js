fcceb7a31c4cf321dfca201fd9034c10db1ff76e
syntactic-salt <kennethpereira@gmail.com>
2021-09-30 19:56:09 -0400

    Add jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,19 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:node/recommended',
+        'plugin:jsdoc/recommended',
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:prettier/recommended',
     ],
     parserOptions: {
         ecmaVersion: 2019,
+    },
+    plugins: ['jsdoc'],
+    rules: {
+        'jsdoc/require-jsdoc': [
+            1,
+            { publicOnly: true, require: { ArrowFunctionExpression: true } },
+        ],
     },
 };
